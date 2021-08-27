96863b92edf809d10924d7da9531503eb8fa81d3
b-kip <bruzkip@gmail.com>
2021-08-27 18:27:33 +0300

    Testimonials complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 const overlay = document.getElementById('js-overlay');
 const menuButton = document.getElementById('js-menu-button');
 const nav = document.getElementById('js-nav');
+import Glide from "@glidejs/glide";
 
 menuButton.addEventListener('click', function(){
   console.log('Button Clicked!')
@@ -11,6 +12,39 @@ menuButton.addEventListener('click', function(){
 });
 
 console.log("JS added!");
+
+new Glide('.glide',
+  {
+    type: 'carousel',
+    startAt: 1,
+    gap: 24,
+    autoplay: 5000,
+    // hoverpause: true,
+    // keyboard: true,
+    throttle: 50,
+    perView: 3,
+    peek: -120,
+    breakpoints: {
+      1000: {
+        perView: 2,
+        peek: 0,
+      },
+      700: {
+        perView: 1,
+        peek: 0,
+      },
+      375: {
+        perView: 1,
+        peek: 0,
+      }
+    },
+    classes: {
+      // activeNav: "testimonials__controls__button--active",
+      activeNav: "glide__bullet--active",
+    }
+  }
+)
+.mount()
 
 if (module.hot) {
   module.hot.accept();
